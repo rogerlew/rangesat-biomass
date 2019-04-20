980089b47439fe7ba76f6efdaaf09aae20ff3d50
Roger Lew <rogerlew@gmail.com>
2019-04-20 16:59:56 -0700

    moved get_sf_wgs_bounds to process_scenes script

--- biomass/rangesat_biomass.py
+++ biomass/rangesat_biomass.py
@@ -9,13 +9,9 @@ from math import sqrt
 from os.path import join as _join
 from os.path import exists as _exists
 
-import pyproj
-import fiona
 from rasterio.mask import raster_geometry_mask
 
 import numpy as np
-from osgeo import osr
-
 from .landsat import LandSatScene
 
 
@@ -256,35 +252,3 @@ class BiomassModel(object):
                             model_stats=model_stats))
 
         return res
-
-
-def wkt_2_proj4(wkt):
-    srs = osr.SpatialReference()
-    srs.ImportFromWkt(wkt)
-    return srs.ExportToProj4().strip()
-
-
-wgs84_proj4 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
-
-
-def get_sf_wgs_bounds(_sf_fn):
-    """
-    returns the bbox of the features in a fiona.shapefile
-    """
-    _sf = fiona.open(_sf_fn, 'r')
-    bboxs = []
-    for feature in _sf:
-        bboxs.append(fiona.bounds(feature))
-
-    bboxs = np.array(bboxs)
-    e, s, w, n = [np.min(bboxs[:, 0]), np.min(bboxs[:, 1]),
-                  np.max(bboxs[:, 2]), np.max(bboxs[:, 3])]
-
-    proj_wkt = open(_sf_fn.replace('.shp', '') + '.prj').read()
-    sf_proj4 = wkt_2_proj4(proj_wkt)
-    sf_proj = pyproj.Proj(sf_proj4)
-    wgs_proj = pyproj.Proj(wgs84_proj4)
-    e, s = pyproj.transform(sf_proj, wgs_proj, e, s)
-    w, n = pyproj.transform(sf_proj, wgs_proj, w, n)
-
-    return e, s, w, n
