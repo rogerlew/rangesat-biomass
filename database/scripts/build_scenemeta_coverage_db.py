e02c443530a67d3948ff52f5cdbf1c5c7d5e1df6
Roger Lew <rogerlew@gmail.com>
2020-11-09 13:58:38 -0800

    build_scenemeta_coverage_db.py optimization with multiprocessing

--- database/scripts/build_scenemeta_coverage_db.py
+++ database/scripts/build_scenemeta_coverage_db.py
@@ -9,58 +9,55 @@ from os.path import join as _join
 from os.path import exists
 import shutil
 import sys
+import multiprocessing
 
 sys.path.insert(0, '/var/www/rangesat-biomass')
 from database.pasturestats import query_scene_product_ids
 
 
-def query_scene_coverage(db_fn, product_id):
+NCPU = multiprocessing.cpu_count() - 1
+
+# db_fn = '/space/rangesat/Zumwalt/sqlite3.db'
+# db_fn = '/var/www/rangesat-biomass/sites/SageSteppe/rcr_sqlite3.db'
+db_fn = '/space/rangesat/db/Zumwalt2/sqlite3.db'
+
+# cov_db_fn = '/space/rangesat/Zumwalt/scenemeta_coverage.db'
+# cov_db_fn = '/var/www/rangesat-biomass/sites/SageSteppe/rcr_scenemeta_coverage.db'
+cov_db_fn = '/space/rangesat/db/Zumwalt2/scenemeta_coverage.db'
+
+conn = sqlite3.connect(db_fn)
+c = conn.cursor()
+
+query = 'SELECT product_id, coverage FROM pasture_stats'
+
+c.execute(query)
+rows = c.fetchall()
+
+
+def calc_scene_coverage(product_id):
     """
     determines the fraction of pastures with coverage over the coverage_threshold_fraction
     """
-    conn = sqlite3.connect(db_fn)
-    c = conn.cursor()
+    x = 0.0
+    n = 0
+    for row in rows:
+        if product_id == row[0]:
+            v = row[1]
+            if v is not None:
+                x += v
+            n += 1
 
-    query = 'SELECT biomass_mean_gpm FROM pasture_stats WHERE product_id = "{product_id}"'\
-            .format(product_id=product_id)
-
-    c.execute(query)
-    rows = c.fetchall()
-    res = [row[0] is not None for row in rows]
-    return sum(res) / len(res)
-
-
-def query_scenes_coverage(scn_cov_db_fn, product_ids):
-    conn = sqlite3.connect(scn_cov_db_fn)
-    c = conn.cursor()
-
-    query = 'SELECT * FROM scenemeta_coverage'
-
-    c.execute(query)
-    rows = c.fetchall()
-    d = {product_id: coverage for product_id, coverage in rows}
-
-    res = []
-    for product_id in product_ids:
-        res.append(d.get(product_id, 0.0))
-
-    return res
+    return product_id, x / float(n)
 
 
 if __name__ == "__main__":
-    #db_fn = '/space/rangesat/Zumwalt/sqlite3.db'
-    db_fn = '/var/www/rangesat-biomass/sites/SageSteppe/rcr_sqlite3.db'
 
-    #cov_db_fn = '/space/rangesat/Zumwalt/scenemeta_coverage.db'
-    cov_db_fn = '/var/www/rangesat-biomass/sites/SageSteppe/rcr_scenemeta_coverage.db'
+    pool = multiprocessing.Pool(NCPU)
 
     if _exists(cov_db_fn):
         os.remove(cov_db_fn)
 
     product_ids = query_scene_product_ids(db_fn)
-
-    #print(query_scenes_coverage(cov_db_fn, product_ids))
-    #sys.exit()
 
     conn = sqlite3.connect(cov_db_fn)
     c = conn.cursor()
@@ -69,9 +66,7 @@ if __name__ == "__main__":
     c.execute("""CREATE TABLE scenemeta_coverage
                  (product_id TEXT, coverage REAL)""")
 
-    for product_id in product_ids:
-
-        coverage = query_scene_coverage(db_fn, product_id)
+    for product_id, coverage in pool.imap_unordered(calc_scene_coverage, product_ids):
         print(product_id, coverage)
 
         query = 'INSERT INTO scenemeta_coverage VALUES ("{product_id}",{coverage})'\
